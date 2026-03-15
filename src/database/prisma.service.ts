4010e6a05b0bab533941b17d7ec56efca5d74a15
Raghad Shoaib <raghadshoaib5@gmail.com>
2026-03-16 02:17:37 +0300

    Setup initial clean architecture with users and auth modules

---
+++ src/database/prisma.service.ts
@@ -0,0 +1,17 @@
+//---------------un comment this code to use prisma------------------
+// import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+// import { PrismaClient } from '@prisma/client';
+
+// @Injectable()
+// export class PrismaService
+//   extends PrismaClient
+//   implements OnModuleInit, OnModuleDestroy
+// {
+//   async onModuleInit() {
+//     await this.$connect();
+//   }
+
+//   async onModuleDestroy() {
+//     await this.$disconnect();
+//   }
+// }
